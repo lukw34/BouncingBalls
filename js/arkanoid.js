162f376784746519169e91f7408a01ca193ed0dd
lukw34 <lukw34@gmail.com>
2018-01-25 20:02:03 +0100

    Init bouncing logic

--- js/arkanoid.js
+++ js/arkanoid.js
@@ -1,5 +1,23 @@
-const canvas = document.getElementsByTagName("canvas"),
-    ctx = canvas.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
+const canvas = document.getElementById("canvas"),
+   ctx = canvas.getContext("2d");
+
+class Ball {
+    constructor(radius, {x = radius, y = radius} = {}) {
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+    }
+
+    init() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+
+    move() {
+        
+    }
+}
+
+const myBall = new Ball(30);
+myBall.init();
